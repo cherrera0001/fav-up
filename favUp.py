7b9cd472220bf9492db13b83c8a9558736ecaee4
cherrera0001 <herrera.jara.cristobal@gmail.com>
2023-03-01 15:49:30 -0300

    Update favUp.py
    Se mejora el código para que puedas hacer proxy de la solicitud que estás enviando y probar desde otra herramienta.

--- favUp.py
+++ favUp.py
@@ -1,30 +1,51 @@
+import mmh3
+import requests
+import codecs
+import sys
 import shodan
 
-# Configurar la clave de API de Shodan
-SHODAN_API_KEY = "TU_SHODAN_API_KEY"
-api = shodan.Shodan(SHODAN_API_KEY)
 
-# Consultar el hash de un sitio web
-def get_favicon_hash(site_url):
+SHODAN_API_KEY = "Aqui_su_key"
+
+def main():
+    if len(sys.argv) < 2:
+        print("[!] Error!")
+        print(f"[-] Use: python3 {sys.argv[0]} <hostname>")
+        sys.exit()
+
+    # Buscar favicon en el host especificado
+    favicon_url = f"http://{sys.argv[1]}/favicon.ico"
+    favicon_data = get_favicon(favicon_url)
+
+    favicon = codecs.encode(favicon_data, "base64")
+    hash_favicon = mmh3.hash(favicon)
+
+    # Realizar consulta a Shodan para encontrar hosts con el mismo favicon
+    api = shodan.Shodan(SHODAN_API_KEY)
+    query = f"http.favicon.hash:{hash_favicon}"
+    results = api.search(query)
+
+    # Obtener dirección IP real del host detrás de Cloudflare
+    for result in results["matches"]:
+        ip = result["ip_str"]
+        headers = {"Host": sys.argv[1]}
+        url = f"http://{ip}/favicon.ico"
+        try:
+            response = requests.get(url, headers=headers, verify=False, proxies={"http": "http://127.0.0.1:8080"})
+            if response.status_code == 200 and response.content == favicon_data:
+                print(f"[+] Hostname: {sys.argv[1]}")
+                print(f"[+] IP address: {ip}")
+                break
+        except:
+            pass
+
+def get_favicon(favicon_url):
     try:
-        # Definir la cadena de consulta
-        query = f"http.favicon.hash:<hash> {site_url}"
-        # Buscar el hash del favicon en la base de datos de Shodan
-        results = api.search(query)
-        # Extraer el hash del favicon del primer resultado (si existe)
-        if len(results['matches']) > 0:
-            favicon_hash = results['matches'][0]['http']['favicon']['hash']
-            return favicon_hash
-        else:
-            return None
-    except shodan.exception.APIError as e:
-        print(f"Error al consultar la API de Shodan: {e}")
-        print("Sugerencia: asegúrate de que tu clave de API sea válida y que la cadena de consulta sea correcta.")
+        response = requests.get(favicon_url, verify=False, proxies={"http": "http://127.0.0.1:8080"})
+        if response.status_code == 200:
+            return response.content
+    except:
+        pass
 
-# Ejemplo de uso
-site_url = "web-sote"
-favicon_hash = get_favicon_hash(site_url)
-if favicon_hash is not None:
-    print(f"Hash del favicon de {site_url}: {favicon_hash}")
-else:
-    print(f"No se pudo encontrar el hash del favicon de {site_url}.")
+if _name_ == '_main_':
+    main()
